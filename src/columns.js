ad06d1d7186ecc7c018078aebf730effbb51e9e3
rod <rod.a.nato@gmail.com>
2018-02-21 23:18:14 -0500

    WIP

--- src/columns.js
+++ src/columns.js
@@ -2,30 +2,37 @@ import React from 'react';
 
 let columnClass = '';
 
-let ColumnsGroup = ({columns, action, row, availablePositions, missedShots}) => {
-  return columns
-    .map(column => {
-      console.log(availablePositions);
-      if (missedShots.includes(row + ',' + column)) {
-        columnClass = 'missed';
-      } else if (availablePositions.includes(row + ',' + column)) {
-        columnClass = 'hit';
-      } else {
-        columnClass = '';
-      }
-      return column;
-    })
-    .map((column, i) => {
-      return (
-        <td data-column={column}
-            key={i}
-            className={columnClass}
-            onClick={() => {
-              action(column, row)
-            }}>
-        </td>
-      );
-    });
-};
+class ColumnsGroup extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return this.props.columns
+      .map(column => {
+        console.log(this.props.availablePositions);
+        console.log(this.props.missedShots);
+        if (this.props.missedShots.includes(row + ',' + column)) {
+          columnClass = 'missed';
+        } else if (this.props.availablePositions.includes(row + ',' + column)) {
+          columnClass = 'hit';
+        } else {
+          columnClass = '';
+        }
+        return column;
+      })
+      .map((column, i) => {
+        return (
+          <td data-column={column}
+              key={i}
+              className={columnClass}
+              onClick={() => {
+                action(column, row)
+              }}>
+          </td>
+        );
+      });
+  }
+}
 
 export {ColumnsGroup};
